a1d157aa96b8ef446e9f1097be5866cce10591ed
casio0128-dev <sweet.cafe0141@gmail.com>
2021-02-09 08:06:27 +0900

    menuコンポーネントが読み込まれるよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from "./component/common/Menu";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Menu />
     </div>
   );
 };
