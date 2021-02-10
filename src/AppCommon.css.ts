e33d89807eb5d0515d4b7e0f386a0ff971351b02
casio0128-dev <sweet.cafe0141@gmail.com>
2021-02-10 09:42:51 +0900

    スタイル定義のファイルを作成し、それぞれ分割するよう修正

---
+++ src/AppCommon.css.ts
@@ -0,0 +1,11 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+}));
+
+export default useStyles;
